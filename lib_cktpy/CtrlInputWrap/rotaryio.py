26e59cafeb550a1ff154c94e196eaefa8ab5a8c4
Alain <alainman.krh@gmail.com>
2024-10-05 17:25:03 -0400

    Reset position

--- lib_cktpy/CtrlInputWrap/rotaryio.py
+++ lib_cktpy/CtrlInputWrap/rotaryio.py
@@ -3,6 +3,12 @@
 from MyState.CtrlInputs.RotEncoders import EncoderSensorIF
 from rotaryio import IncrementalEncoder
 
+r"""HACK INFO (Circuit Python 9.1.4)
+- IncrementalEncoder keeps incrementing all the way to +/-maxint ((1<<31)-1).
+- Will roll over to negative values (and vice-versa) - will NOT saturate.
+- HACK: reset .position = 0 after reading. WARN: Might swallow some readings.
+- Hack necessary because Python largeint behaves strangely @ roll-over.
+"""
 
 #==EncoderSensorRIO
 #===============================================================================
@@ -11,12 +17,13 @@ class EncoderSensorRIO(EncoderSensorIF):
 	def __init__(self, pin_a, pin_b, scale=1):
 		self.sense = IncrementalEncoder(pin_a, pin_b)
 		self.scale = -scale #IncrementalEncoder positions decrease in clockwise direction.
-		self.poslast = 0
 		self.read_delta() #Read to zero out initial postition
 
 	def read_delta(self):
-		"""From last time checked"""
-		pos = self.sense.position #Assuming encoder rolls over??
-		delta = pos - self.poslast #Magnitude can be >1 if multiple clicks processed between calls.
-		self.poslast = pos
+		"""From last time checked (resets .position)"""
+		#Magnitude can be >1 if multiple clicks processed between calls:
+		delta = self.sense.position
+		if delta != 0:
+			self.sense.position = 0 #Hack: rolls over @ (1<<31)-1 (never saturates).
+			#Don't reset position unless delta found (less likely to miss a click)
 		return delta*self.scale
